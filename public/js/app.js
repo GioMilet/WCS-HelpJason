e6986a5c59a1076aea565d035fb842c26b40d1f3
Giovanni Milet <miletgiovanni@gmail.com>
2020-09-15 12:49:46 +0200

    Change website title

--- public/js/app.js
+++ public/js/app.js
@@ -1,106 +1,104 @@
 let app = {
-    
-  fetchOptions : {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'no-cache',
-      headers:{
-          'Accept':'application/json',
-      },
-  },
 
-  baseUrl: 'http://127.0.0.1:8000/api',
+    fetchOptions: {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+            'Accept': 'application/json',
+        },
+    },
 
-  init: function(){
-      app.loadCrewMembers();
-      document.querySelector('.new-member-form').addEventListener('submit', app.addNewMember);
+    baseUrl: 'http://127.0.0.1:8000/api',
 
-  },
+    init: function () {
+        app.loadCrewMembers();
+        document.querySelector('.new-member-form').addEventListener('submit', app.addNewMember);
 
-  loadCrewMembers: function(){
-  fetch(app.baseUrl, app.fetchOptions)
-      .then(
-          function(response){
-              return response.json();
-          }
-      )
-      .then(
-          function(crewMembers) {
-              let listMember = document.querySelector('.member-list');
-              crewMembers.forEach(
-                  function(member){
-                      let divMember = document.createElement('div');
-                      let name = member['name'];
-                      if(name.length >= 3) {
-                          //take the letter at the index 0 (first letter) cacapitalize then with slice you take the string from the second letter (index 1)
-                          divMember.innerHTML = name.charAt(0).toUpperCase() + name.slice(1)
-                          divMember.classList.add('member-item');
-                          listMember.appendChild(divMember);
-                      }
-              });
-          }
-      );
-},
+    },
 
-
-addNewMember: function(evt){
-
-evt.preventDefault();
+    loadCrewMembers: function () {
+        fetch(app.baseUrl, app.fetchOptions)
+            .then(
+                function (response) {
+                    return response.json();
+                }
+            )
+            .then(
+                function (crewMembers) {
+                    let listMember = document.querySelector('.member-list');
+                    crewMembers.forEach(
+                        function (member) {
+                            let divMember = document.createElement('div');
+                            let name = member['name'];
+                            if (name.length >= 3) {
+                                //take the letter at the index 0 (first letter) cacapitalize then with slice you take the string from the second letter (index 1)
+                                divMember.innerHTML = name.charAt(0).toUpperCase() + name.slice(1)
+                                divMember.classList.add('member-item');
+                                listMember.appendChild(divMember);
+                            }
+                        });
+                }
+            );
+    },
 
 
-let listMember = document.querySelector('.member-list');
-let input = document.querySelector('input');
-let newName = input.value
-let addMember = document.createElement('div');
-let divErr = document.querySelector('.error')
-let errMsg = document.createElement('p');
-divErr.innerHTML = '';
+    addNewMember: function (evt) {
 
-let data = {
-  name: newName,
-};
+        evt.preventDefault();
 
-let myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
 
-let ajaxOptions = app.fetchOptions
-  ajaxOptions.method = 'POST',
-  ajaxOptions.headers= myHeaders,
-  ajaxOptions.body = JSON.stringify(data)
-  fetch(app.baseUrl, ajaxOptions)
-  .then(function (response) {
-          if (response.status == 201) {                       
-              return response.json()
-          } else {
-              alert('We\'ve got a problem here');
-          }
-      }
-  )
-  .then(
-      function (newMember) {
-          //newMember return an array but we just need the property 'name'
-          name = newMember['name'];
-          if(name.length == 0 ){
-              errMsg.innerHTML = 'Il n\'a pas de nom ton argonaute?';
-              errMsg.style.color = 'red';
-              divErr.appendChild(errMsg);
-              input.value = '';
-          }
-          else if(name.length > 0 && name.length < 3){
-              errMsg.innerHTML = "C'est pas un peu court comme nom '" + name.charAt(0).toUpperCase() + name.slice(1) + "' ?" ;
-              errMsg.style.color = 'red';
-              console.log(errMsg)
-              divErr.appendChild(errMsg);
-              input.value = '';
-              
-          } else {
-              addMember.innerHTML = name
-              addMember.classList.add('member-item');
-              listMember.appendChild(addMember);
-              input.value = '';
-              
-          };
-      })
-  }
+        let listMember = document.querySelector('.member-list');
+        let input = document.querySelector('input');
+        let newName = input.value
+        let addMember = document.createElement('div');
+        let divErr = document.querySelector('.error')
+        let errMsg = document.createElement('p');
+        divErr.innerHTML = '';
+
+        let data = {
+            name: newName,
+        };
+
+        let myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        let ajaxOptions = app.fetchOptions
+        ajaxOptions.method = 'POST',
+            ajaxOptions.headers = myHeaders,
+            ajaxOptions.body = JSON.stringify(data)
+        fetch(app.baseUrl, ajaxOptions)
+            .then(function (response) {
+                if (response.status == 201) {
+                    return response.json()
+                } else {
+                    alert('We\'ve got a problem here');
+                }
+            })
+            .then(
+                function (newMember) {
+                    //newMember return an array but we just need the property 'name'
+                    name = newMember['name'];
+                    if (name.length == 0) {
+                        errMsg.innerHTML = 'Il n\'a pas de nom ton argonaute?';
+                        errMsg.style.color = 'red';
+                        divErr.appendChild(errMsg);
+                        input.value = '';
+                    } else if (name.length > 0 && name.length < 3) {
+                        errMsg.innerHTML = "C'est pas un peu court comme nom '" + name.charAt(0).toUpperCase() + name.slice(1) + "' ?";
+                        errMsg.style.color = 'red';
+                        console.log(errMsg)
+                        divErr.appendChild(errMsg);
+                        input.value = '';
+
+                    } else {
+                        addMember.innerHTML = name
+                        addMember.classList.add('member-item');
+                        listMember.appendChild(addMember);
+                        input.value = '';
+
+                    };
+                })
+    }
 }
 app.init();
